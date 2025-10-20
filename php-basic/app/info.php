624f6c623de72981e418f2ddb0f9870e37dd52a9
davi308 <davi308@vidalibarraquer.net>
2025-10-20 11:40:25 +0000

    Scripts base de datos y ReadMe

--- php-basic/app/info.php
+++ php-basic/app/info.php
@@ -1,5 +1,5 @@
 <?
-echo "<h1>El servicio PHP esta funcionando! </h1>"
-echo "<p>Grup: Daniel, Jordi, Sebastian</p>"
+echo "<h1>El servicio PHP esta funcionando! </h1>";
+echo "<p>Grup: Daniel, Jordi, Sebastian</p>";
 phpinfo();
 ?>
